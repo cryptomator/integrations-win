30185464eceaba11aca2e98b435ba20de2920f80
Armin Schrenk <armin.schrenk@skymatic.de>
2020-12-01 17:03:47 +0100

    Replace creation of shortcut via powershell with native method.

--- src/main/java/org/cryptomator/windows/autostart/WindowsAutoStart.java
+++ src/main/java/org/cryptomator/windows/autostart/WindowsAutoStart.java
@@ -10,9 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
-import static org.cryptomator.windows.autostart.AutoStartUtil.waitForProcessOrCancel;
 
 /**
  * Checks, en- and disables autostart for Cryptomator on Windows using the startup folder.
@@ -24,19 +21,20 @@ public class WindowsAutoStart implements AutoStartProvider {
 	private static final Logger LOG = LoggerFactory.getLogger(WindowsAutoStart.class);
 	private static final String RELATIVE_STARTUP_FOLDER_ENTRY = "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Cryptomator.lnk";
 
+	private final WinShortcutCreation winShortcutCreation;
 	private final Path absoluteStartupEntryPath;
 	private final Optional<String> exePath;
 
 	@SuppressWarnings("unused") // default constructor required by ServiceLoader
 	public WindowsAutoStart() {
+		this.winShortcutCreation = new WinShortcutCreation();
 		this.exePath = ProcessHandle.current().info().command();
 		this.absoluteStartupEntryPath = Path.of(System.getProperty("user.home"), RELATIVE_STARTUP_FOLDER_ENTRY).toAbsolutePath();
 	}
 
 	@Override
 	public boolean isEnabled() {
-		Path autoStartEntry = Path.of(System.getProperty("user.home"), RELATIVE_STARTUP_FOLDER_ENTRY);
-		return Files.exists(autoStartEntry);
+		return Files.exists(absoluteStartupEntryPath);
 	}
 
 	@Override
@@ -46,20 +44,11 @@ public class WindowsAutoStart implements AutoStartProvider {
 		}
 
 		assert exePath.isPresent();
-		String createShortcutCommand = "$s=(New-Object -COM WScript.Shell).CreateShortcut('" + absoluteStartupEntryPath.toString() + "');$s.TargetPath='" + exePath.get() + "';$s.Save();";
-		ProcessBuilder shortcutAdd = new ProcessBuilder("cmd", "/c", "Start powershell " + createShortcutCommand);
-		try {
-			Process proc = shortcutAdd.start();
-			boolean finishedInTime = waitForProcessOrCancel(proc, 5, TimeUnit.SECONDS);
-			if (finishedInTime && proc.exitValue() == 0) {
-				LOG.debug("Created file {} for auto start.", absoluteStartupEntryPath);
-				return;
-			} else {
-				LOG.debug("Adding entry to auto start folder failed.");
-				throw new IOException("Process exited with error code " + proc.exitValue());
-			}
-		} catch (IOException e) {
-			throw new ToggleAutoStartFailedException("Enabling autostart using the startup folder failed.", e);
+		int returnCode = winShortcutCreation.createShortcut(exePath.get(), absoluteStartupEntryPath.toString(), "Cryptomator");
+		if (returnCode == 0) {
+			return;
+		} else {
+			throw new ToggleAutoStartFailedException("Enabling autostart using the startup folder failed. Windows error code: " + returnCode);
 		}
 	}
 
