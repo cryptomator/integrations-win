16130f5d0d39b8abe08bcdac8a387c20ccfc52e5
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-03-07 14:01:37 +0100

    fix test

--- src/test/java/org/cryptomator/windows/keychain/KeychainAccessProviderTest.java
+++ src/test/java/org/cryptomator/windows/keychain/KeychainAccessProviderTest.java
@@ -2,14 +2,27 @@ package org.cryptomator.windows.keychain;
 
 import org.cryptomator.integrations.keychain.KeychainAccessProvider;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Path;
 
 public class KeychainAccessProviderTest {
+
+	@BeforeAll
+	public static void setup(@TempDir Path tmpDir) {
+		Path keychainPath = tmpDir.resolve("keychain.tmp");
+		System.setProperty("cryptomator.keychainPath", keychainPath.toString());
+	}
 
 	@Test
 	@DisplayName("WindowsProtectedKeychainAccess can be loaded")
 	public void testLoadWindowsProtectedKeychainAccess() {
+		Assumptions.assumeFalse(System.getProperty("cryptomator.keychainPath", "").isBlank());
+		
 		var windowsKeychainAccessProvider = KeychainAccessProvider.get().findAny();
 		Assertions.assertTrue(windowsKeychainAccessProvider.isPresent());
 		Assertions.assertInstanceOf(WindowsProtectedKeychainAccess.class, windowsKeychainAccessProvider.get());
