5354d2f5a629f0c7c3aa488d77f4eda1eabadcb6
Armin Schrenk <armin.schrenk@skymatic.de>
2022-05-18 14:03:02 +0200

    Fixes #13

--- src/test/java/org/cryptomator/windows/keychain/KeychainAccessProviderTest.java
+++ src/test/java/org/cryptomator/windows/keychain/KeychainAccessProviderTest.java
@@ -1,13 +1,18 @@
 package org.cryptomator.windows.keychain;
 
+import org.cryptomator.integrations.keychain.KeychainAccessException;
 import org.cryptomator.integrations.keychain.KeychainAccessProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class KeychainAccessProviderTest {
@@ -22,10 +27,39 @@ public class KeychainAccessProviderTest {
 	@DisplayName("WindowsProtectedKeychainAccess can be loaded")
 	public void testLoadWindowsProtectedKeychainAccess() {
 		Assumptions.assumeFalse(System.getProperty("cryptomator.keychainPath", "").isBlank());
-		
+
 		var windowsKeychainAccessProvider = KeychainAccessProvider.get().findAny();
 		Assertions.assertTrue(windowsKeychainAccessProvider.isPresent());
 		Assertions.assertInstanceOf(WindowsProtectedKeychainAccess.class, windowsKeychainAccessProvider.get());
+	}
+
+	@Nested
+	public class LoadKeyChainEntries {
+
+		Path keychainPath;
+		WindowsProtectedKeychainAccess keychainAccess;
+
+		@BeforeEach
+		public void init() throws IOException {
+			keychainPath = Path.of(System.getProperty("cryptomator.integrationsWin.keychainPaths"));
+			Files.write(keychainPath, new byte[] {});
+			keychainAccess = (WindowsProtectedKeychainAccess) KeychainAccessProvider.get().findAny().get();
+		}
+
+		@Test
+		public void testNonExistingFileReturnsEmpty() throws KeychainAccessException, IOException {
+			Files.delete(keychainPath);
+			var result = keychainAccess.loadKeychainEntries(keychainPath);
+
+			Assertions.assertTrue(result.isEmpty());
+		}
+
+		@Test
+		public void testEmptyFileReturnsEmpty() throws KeychainAccessException {
+			var result = keychainAccess.loadKeychainEntries(keychainPath);
+
+			Assertions.assertTrue(result.isEmpty());
+		}
 	}
 
 }
