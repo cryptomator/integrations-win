b4827ff2d7d0eed0d40f95d0f564060de92a2f77
Armin Schrenk <armin.schrenk@skymatic.de>
2023-09-14 16:59:03 +0200

    rely on default jackson mapper settings

--- src/main/java/org/cryptomator/windows/keychain/KeychainEntry.java
+++ src/main/java/org/cryptomator/windows/keychain/KeychainEntry.java
@@ -2,15 +2,6 @@ package org.cryptomator.windows.keychain;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-record KeychainEntry(@JsonProperty("ciphertext") //
-					 @JsonSerialize(using = ByteArrayJsonAdapter.Serializer.class) //
-					 @JsonDeserialize(using = ByteArrayJsonAdapter.Deserializer.class) //
-					 byte[] ciphertext,
-					 @JsonProperty("salt") //
-					 @JsonSerialize(using = ByteArrayJsonAdapter.Serializer.class) //
-					 @JsonDeserialize(using = ByteArrayJsonAdapter.Deserializer.class) //
-					 byte[] salt) {
+record KeychainEntry(@JsonProperty("ciphertext") byte[] ciphertext, @JsonProperty("salt") byte[] salt) {
 }
