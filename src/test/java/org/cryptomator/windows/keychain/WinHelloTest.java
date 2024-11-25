1dc2adbcdd7104322400377957fd1f979a5f7553
Armin Schrenk <armin.schrenk@skymatic.de>
2024-11-25 15:48:14 +0100

    refactor WinHelloTest to be more unit like

--- src/test/java/org/cryptomator/windows/keychain/WinHelloTest.java
+++ src/test/java/org/cryptomator/windows/keychain/WinHelloTest.java
@@ -1,41 +1,32 @@
 package org.cryptomator.windows.keychain;
 
 import org.cryptomator.integrations.keychain.KeychainAccessException;
-import org.cryptomator.integrations.keychain.KeychainAccessProvider;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org.junit.jupiter.api.io.TempDir;
-
-import java.nio.file.Path;
 
 @EnabledOnOs(OS.WINDOWS)
 @DisabledIfEnvironmentVariable(named = "CI", matches = ".*")
 class WinHelloTest {
 
-	static WindowsProtectedKeychainAccess keychain;
-
-	@BeforeAll
-	public static void setup(@TempDir Path tmpDir) {
-		Path keychainPath = tmpDir.resolve("keychain.tmp");
-		System.setProperty("cryptomator.integrationsWin.keychainPaths", keychainPath.toString());
-		keychain = (WindowsProtectedKeychainAccess) KeychainAccessProvider.get().findAny().get();
-
-	}
-
 	@Test
 	@DisplayName("Test Windows Hello Authentication")
 	@Disabled
 	public void testStoreAndLoadWithAuth() throws KeychainAccessException {
-		String storedPw = "secretPassword";
-		// Test with authentication required
-		keychain.storePassphrase("securePassword", null, storedPw, true);
-		String loadedPw = new String(keychain.loadPassphrase("securePassword"));
-		Assertions.assertEquals(storedPw, loadedPw);
+		var winhello = new WinHello();
+		String storedPw = "h€llo wørld123";
+		byte[] ciphertext = winhello.setEncryptionKey(storedPw.getBytes(), "salt".getBytes());
+		Assertions.assertNotNull(ciphertext);
+
+		byte[] shouldBeNull = winhello.getEncryptionKey(ciphertext, "pepper".getBytes());
+		Assertions.assertNull(shouldBeNull);
+
+		byte[] cleartext = winhello.getEncryptionKey(ciphertext, "salt".getBytes());
+		Assertions.assertArrayEquals(storedPw.getBytes(), cleartext);
 	}
+
 }
