1902053ab6bdcefefb337194ca3f2075e4c3f8d6
Armin Schrenk <armin.schrenk@skymatic.de>
2024-12-03 15:55:03 +0100

    WindowsHello and DPAPI extend the FileKeychainAccessProvider

--- src/main/java/org/cryptomator/windows/keychain/WindowsProtectedKeychainAccess.java
+++ src/main/java/org/cryptomator/windows/keychain/WindowsProtectedKeychainAccess.java
@@ -2,20 +2,8 @@ package org.cryptomator.windows.keychain;
 
 import org.cryptomator.integrations.common.OperatingSystem;
 import org.cryptomator.integrations.common.Priority;
-import org.cryptomator.integrations.keychain.KeychainAccessException;
 import org.cryptomator.integrations.keychain.KeychainAccessProvider;
 import org.cryptomator.windows.common.Localization;
-
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Windows implementation for the {@link KeychainAccessProvider} based on the <a href="https://en.wikipedia.org/wiki/Data_Protection_API">data protection API</a>.
@@ -23,37 +11,21 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 @Priority(1000)
 @OperatingSystem(OperatingSystem.Value.WINDOWS)
-public class WindowsProtectedKeychainAccess implements KeychainAccessProvider {
+public final class WindowsProtectedKeychainAccess extends WindowsFileKeychainAccess {
 
 	private static final String KEYCHAIN_PATHS_PROPERTY = "cryptomator.integrationsWin.keychainPaths";
 
-	private final List<Path> keychainPaths;
 	private final WinDataProtection dataProtection;
-	private Map<String, KeychainEntry> keychainEntries;
 
 	@SuppressWarnings("unused") // default constructor required by ServiceLoader
 	public WindowsProtectedKeychainAccess() {
-		this(readKeychainPathsFromEnv(), new WinDataProtection());
+		this(KEYCHAIN_PATHS_PROPERTY, new WinDataProtection());
 	}
 
 	// visible for testing
-	WindowsProtectedKeychainAccess(List<Path> keychainPaths, WinDataProtection dataProtection) {
-		this.keychainPaths = keychainPaths;
+	WindowsProtectedKeychainAccess(String keychainPathsProp, WinDataProtection dataProtection) {
+		super(keychainPathsProp);
 		this.dataProtection = dataProtection;
-	}
-
-	private static List<Path> readKeychainPathsFromEnv() {
-		var keychainPaths = System.getProperty(KEYCHAIN_PATHS_PROPERTY, "");
-		return parsePaths(keychainPaths, System.getProperty("path.separator"));
-	}
-
-	// visible for testing
-	static List<Path> parsePaths(String listOfPaths, String pathSeparator) {
-		return Arrays.stream(listOfPaths.split(pathSeparator))
-				.filter(Predicate.not(String::isEmpty))
-				.map(Path::of)
-				.map(Util::resolveHomeDir)
-				.collect(Collectors.toList());
 	}
 
 	@Override
@@ -62,56 +34,8 @@ public class WindowsProtectedKeychainAccess implements KeychainAccessProvider {
 	}
 
 	@Override
-	public void storePassphrase(String key, String displayName, CharSequence passphrase, boolean ignored) throws KeychainAccessException {
-		keychainEntries = Util.loadKeychainEntriesIfNeeded(keychainPaths, keychainEntries);
-		ByteBuffer buf = UTF_8.encode(CharBuffer.wrap(passphrase));
-		byte[] cleartext = new byte[buf.remaining()];
-		buf.get(cleartext);
-		var salt = Util.generateSalt();
-		var ciphertext = dataProtection.protect(cleartext, salt);
-		Arrays.fill(buf.array(), (byte) 0x00);
-		Arrays.fill(cleartext, (byte) 0x00);
-		keychainEntries.put(key, new KeychainEntry(ciphertext, salt));
-		Util.saveKeychainEntries(keychainPaths, keychainEntries);
-	}
-
-	@Override
-	public char[] loadPassphrase(String key) throws KeychainAccessException {
-		keychainEntries = Util.loadKeychainEntriesIfNeeded(keychainPaths, keychainEntries);
-		KeychainEntry entry = keychainEntries.get(key);
-		if (entry == null) {
-			return null;
-		}
-		byte[] cleartext = dataProtection.unprotect(entry.ciphertext(), entry.salt());
-		if (cleartext == null) {
-			return null;
-		}
-		CharBuffer buf = UTF_8.decode(ByteBuffer.wrap(cleartext));
-		char[] passphrase = new char[buf.remaining()];
-		buf.get(passphrase);
-		Arrays.fill(cleartext, (byte) 0x00);
-		Arrays.fill(buf.array(), (char) 0x00);
-		return passphrase;
-	}
-
-	@Override
-	public void deletePassphrase(String key) throws KeychainAccessException {
-		keychainEntries = Util.loadKeychainEntriesIfNeeded(keychainPaths, keychainEntries);
-		keychainEntries.remove(key);
-		Util.saveKeychainEntries(keychainPaths, keychainEntries);
-	}
-
-	@Override
-	public void changePassphrase(String key, String displayName, CharSequence passphrase) throws KeychainAccessException {
-		keychainEntries = Util.loadKeychainEntriesIfNeeded(keychainPaths, keychainEntries);
-		if (keychainEntries.remove(key) != null) {
-			storePassphrase(key, passphrase);
-		}
-	}
-
-	@Override
 	public boolean isSupported() {
-		return !keychainPaths.isEmpty();
+		return super.keychainPathPresent;
 	}
 
 	@Override
@@ -119,4 +43,13 @@ public class WindowsProtectedKeychainAccess implements KeychainAccessProvider {
 		return false;
 	}
 
+	@Override
+	byte[] encrypt(byte[] passphrase, byte[] salt) {
+		return dataProtection.protect(passphrase, salt);
+	}
+
+	@Override
+	byte[] decrypt(byte[] ciphertext, byte[] salt) {
+		return dataProtection.unprotect(ciphertext, salt);
+	}
 }
