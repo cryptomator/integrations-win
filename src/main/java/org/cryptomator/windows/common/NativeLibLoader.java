7618b84419cdc12abc2989a54eb260d650146655
Ralph Plawetzki <ralph@purejava.org>
2025-05-30 10:46:40 +0200

    Make library compatible to Windows ARM64 as well (#106)
    ---------
    
    Co-authored-by: Armin Schrenk <armin.schrenk@skymatic.de>

--- src/main/java/org/cryptomator/windows/common/NativeLibLoader.java
+++ src/main/java/org/cryptomator/windows/common/NativeLibLoader.java
@@ -12,27 +12,42 @@ import java.util.Objects;
 public class NativeLibLoader {
 
 	private static final Logger LOG = LoggerFactory.getLogger(NativeLibLoader.class);
-	private static final String LIB = "/integrations.dll";
+	private static final String X64_LIB = "/integrations-x64.dll";
+	private static final String ARM64_LIB = "/integrations-arm64.dll";
 	private static volatile boolean loaded = false;
 
 	/**
 	 * Attempts to load the .dll file required for native calls.
+	 *
 	 * @throws UnsatisfiedLinkError If loading the library failed.
 	 */
 	public static synchronized void loadLib() {
 		if (!loaded) {
-			try (var dll = NativeLibLoader.class.getResourceAsStream(LIB)) {
+			var arch = System.getProperty("os.arch");
+			final String LIBNAME;
+			if (arch.contains("amd64")) {
+				LOG.debug("Loading library for x86_64 architecture");
+				LIBNAME = X64_LIB;
+			} else if (arch.contains("aarch64")) {
+				LOG.debug("Loading library for aarch64 architecture");
+				LIBNAME = ARM64_LIB;
+			} else {
+				LOG.warn("Unrecognized architecture: {}. Defaulting to x86_64 architecture", arch);
+				LIBNAME = X64_LIB;
+			}
+
+			try (var dll = NativeLibLoader.class.getResourceAsStream(LIBNAME)) {
 				Objects.requireNonNull(dll);
 				Path tmpPath = Files.createTempFile("lib", ".dll");
 				Files.copy(dll, tmpPath, StandardCopyOption.REPLACE_EXISTING);
 				System.load(tmpPath.toString());
 				loaded = true;
 			} catch (NullPointerException e) {
-				LOG.error("Did not find resource " + LIB, e);
+				LOG.error("Did not find resource " + LIBNAME, e);
 			} catch (IOException e) {
-				LOG.error("Failed to copy " + LIB + " to temp dir.", e);
+				LOG.error("Failed to copy " + LIBNAME + " to temp dir.", e);
 			} catch (UnsatisfiedLinkError e) {
-				LOG.error("Failed to load lib from " + LIB, e);
+				LOG.error("Failed to load lib from " + LIBNAME, e);
 			}
 		}
 	}
